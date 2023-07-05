b8b5adb0d40aa2c392508706960477080fbacf37
Marco Damaceno <maadamaceno@gmail.com>
2023-07-05 17:50:08 -0300

    chore: only routes in routes file

--- config/routes.go
+++ config/routes.go
@@ -1,19 +1,12 @@
 package config
 
 import (
-    "path/filepath"
     "github.com/gin-gonic/gin"
 
     "app/web/controller"
 )
 
-func SetupRoutes() *gin.Engine {
-    r := gin.New()
-    r.Use(gin.Logger())
-    r.Use(gin.Recovery())
-    r.Static("/public", "./public")
-    r.LoadHTMLGlob(filepath.Join("web", "resources", "views", "**", "*.html"))
-
+func SetupRoutes(r *gin.Engine) *gin.Engine {
     r.GET("/", controller.HomeIndex)
 
     return r
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "app/config"
+import (
+    "path/filepath"
+    "github.com/gin-gonic/gin"
+
+    "app/config"
+)
 
 func main() {
-    r := config.SetupRoutes()
+    r := gin.New()
+    r.Use(gin.Logger())
+    r.Use(gin.Recovery())
+    r.Static("/public", "./public")
+    r.LoadHTMLGlob(filepath.Join("web", "resources", "views", "**", "*.html"))
+
+    r = config.SetupRoutes(r)
+
     r.Run()
 }
