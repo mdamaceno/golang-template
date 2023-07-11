c380cadd0aacbb8ff181ebf80ec89397ba654cf8
Marco Damaceno <maadamaceno@gmail.com>
2023-07-11 18:16:10 -0300

    refactor: remove yaml file config

--- web/view/view.go
+++ web/view/view.go
@@ -1,56 +1,19 @@
 package view
 
 import (
-    "io/ioutil"
-    "log"
     "path/filepath"
-    "strings"
 
-    "gopkg.in/yaml.v3"
     "github.com/gin-contrib/multitemplate"
 )
 
-var viewConfigFile = filepath.Join("config", "views.yml")
-var defaultLayout = "application"
+var viewsDir = filepath.Join("web", "view")
+var defaultLayoutFile = filepath.Join(viewsDir, "layouts", "application.html")
 
 func SetViews() multitemplate.Renderer {
     r := multitemplate.NewRenderer()
 
-    vcfg := make(map[string]interface{})
-
-    data, err := ioutil.ReadFile(viewConfigFile)
-
-    if err != nil {
-        log.Fatalf("Failed to read YAML file: %v", err)
-    }
-
-    err = yaml.Unmarshal(data, &vcfg)
-
-    if err != nil {
-        log.Fatalf("Failed to unmarshal YAML: %v", err)
-    }
-
-    for m, values := range vcfg {
-        for _, v := range values.([]interface{}) {
-            s := strings.Split(v.(string), ":")
-
-            if len(s) < 2 {
-                r.AddFromFiles(s[0], filepath.Join(m, "view", s[0] + ".html"))
-            } else {
-                l := s[1]
-
-                if l == "" {
-                    l = defaultLayout
-                }
-
-                r.AddFromFiles(
-                    s[0],
-                    filepath.Join(m, "view", "layouts", l + ".html"),
-                    filepath.Join(m, "view", s[0] + ".html"),
-                )
-            }
-        }
-    }
+    r.AddFromFiles("home/index", defaultLayoutFile, viewsDir + "/home/index.html")
+    r.AddFromFiles("users/index", defaultLayoutFile, viewsDir + "/users/index.html")
 
     return r
 }
