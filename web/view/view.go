4ca741a8ae5ba954d170b863ee1cc17f7138cf6e
Marco Damaceno <maadamaceno@gmail.com>
2023-07-12 21:03:35 -0300

    remove multitemplate package

--- web/view/view.go
+++ web/view/view.go
@@ -2,18 +2,62 @@ package view
 
 import (
     "path/filepath"
+    "html/template"
+    "fmt"
 
-    "github.com/gin-contrib/multitemplate"
+    "github.com/gin-gonic/gin/render"
 )
 
-var viewsDir = filepath.Join("web", "view")
-var defaultLayoutFile = filepath.Join(viewsDir, "layouts", "application.html")
+type Render map[string]*template.Template
 
-func SetViews() multitemplate.Renderer {
-    r := multitemplate.NewRenderer()
+type Renderer interface {
+	render.HTMLRender
+	add(name string, tmpl *template.Template)
+	addFromFiles(name string, files ...string) *template.Template
+}
 
-    r.AddFromFiles("home/index", defaultLayoutFile, viewsDir + "/home/index.html")
-    r.AddFromFiles("users/index", defaultLayoutFile, viewsDir + "/users/index.html")
+var (
+	_ render.HTMLRender = Render{}
+	_ Renderer          = Render{}
+    viewsDir            = filepath.Join("web", "view")
+    defaultLayoutFile   = filepath.Join(viewsDir, "layouts", "application.html")
+)
+
+func SetViews() Renderer {
+    r := make(Render)
+
+    r.addFromFiles("home/index", defaultLayoutFile, viewsDir + "/home/index.html")
+    r.addFromFiles("users/index", defaultLayoutFile, viewsDir + "/users/index.html")
 
     return r
+}
+
+func (r Render) add(name string, tmpl *template.Template) {
+	if tmpl == nil {
+		panic("template can not be nil")
+	}
+
+	if len(name) == 0 {
+		panic("template name cannot be empty")
+	}
+
+	if _, ok := r[name]; ok {
+		panic(fmt.Sprintf("template %s already exists", name))
+	}
+
+	r[name] = tmpl
+}
+
+func (r Render) addFromFiles(name string, files ...string) *template.Template {
+	tmpl := template.Must(template.ParseFiles(files...))
+	r.add(name, tmpl)
+
+	return tmpl
+}
+
+func (r Render) Instance(name string, data interface{}) render.Render {
+	return render.HTML{
+		Template: r[name],
+		Data:     data,
+	}
 }
